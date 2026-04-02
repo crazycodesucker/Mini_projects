cc13eb43516d798b01ee0ff33d7b14d3a610d9e9
crazycodesucker <younot2211@gmail.com>
2026-04-02 17:59:22 +0530

    cs50 4th problem

---
+++ einstien.py
@@ -0,0 +1,14 @@
+# /////////////////// Einstein 
+
+def einstien():
+    m = int(input("enter the mass "))
+    e = m * (300000000)**2
+    print(e)
+
+#    /////////// x = 4500000000000000000
+    if x == e:
+        print("result matches")
+    else:
+        print("you got wrong results nigga")
+
+einstien()
