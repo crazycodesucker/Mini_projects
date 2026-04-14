ec14d03d2b2ccc894d5be23a13a4f7b74d6b5a76
crazycodesucker <younot2211@gmail.com>
2026-04-15 01:18:03 +0530

    lec 1 problem 1

---
+++ deepthought.py
@@ -0,0 +1,8 @@
+# ///// ddeep thought v2
+
+que = input("what is the answer to the Great Question of Life, the Universe and Everything ").lower()
+
+if que == "42" or que == "forty-two" or que ==  "forty two":
+    print("Yes...!")
+else:
+    print("No.")
